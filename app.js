56156bc96ce5783e441c2fe4c8100d357891ac52
Yogiraj Patil <yogiraj10033921@gmail.com>
2025-01-29 21:53:53 +0530

    socket io integration

--- app.js
+++ app.js
@@ -8,38 +8,36 @@ import router from './routes/routes.js';
 import { socketHandler } from './utils/socketInstance.js';
 import http from 'http';
 
+
 dotenv.config();
-
 const app = express();
-
-// Environment variables
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json());
-app.use('/api', router);
 
-// Connect to MongoDB
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB connected successfully!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-//socket io conn
-  const server = http.createServer(app);
-  const io = new Server(server);
-  
-  // Handle socket connections
-  socketHandler(io);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.listen(PORT, () => {
+app.use("/api", router);
+socketHandler(io);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

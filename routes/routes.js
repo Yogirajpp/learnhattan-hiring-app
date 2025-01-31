9258295f4acf92ce1c02d3459b75a40b71660d1b
prithvimulay <prithvirajeis@gmail.com>
2025-02-01 01:37:05 +0530

    Waitlist Done for home

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { fetchGithubRepo, getAllProjects } from '../controller/projectContoller.js';
+import { createWaitlistEntry, getWaitlistEntries } from '../controller/waitListController.js';
 
 const router = express.Router();
 
 router.get('/projects', getAllProjects); // Route to fetch all projects
 router.get('/projects/:owner/:repoName', fetchGithubRepo); // Route to fetch a single GitHub repo and save it to MongoDB
-
+    
+router.post('/join', createWaitlistEntry);
+router.get('/entries', getWaitlistEntries);
+  
 export default router;
